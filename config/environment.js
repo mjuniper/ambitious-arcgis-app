123df8945abdff2737bbf867c7ceeb4fd5b58f18
Tom Wayson <tom@tomwayson.com>
2017-03-06 13:37:22 -0800

    addons

--- config/environment.js
+++ config/environment.js
@@ -2,6 +2,14 @@
 
 module.exports = function(environment) {
   var ENV = {
+    torii: {
+      sessionServiceName: 'session',
+      providers: {
+       'arcgis-oauth-bearer': {
+          portalUrl: 'https://www.arcgis.com'
+        }
+      }
+    },
     modulePrefix: 'ambitious-arcgis-app-test',
     environment: environment,
     rootURL: '/',
